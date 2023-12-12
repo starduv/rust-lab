cc52f76ff472af13c2e395779c2ed12b2616e922
Joel C. Rainear-Wills <jrainearwills@gmail.com>
2023-12-12 07:10:50 -0600

    still stumped on 10 part 2

--- src/advent_of_code/_2023_/day_ten/part_two.rs
+++ src/advent_of_code/_2023_/day_ten/part_two.rs
@@ -1,90 +1,51 @@
 use std::collections::BTreeMap;
 
-pub fn run() -> u32 {
-    let north = [-1, 0];
-    let south = [1, 0];
-    let east = [0, 1];
-    let west = [0, -1];
-
-    let features = BTreeMap::from([
-        ('|', Feature::Pipe(north, south)),
-        ('-', Feature::Pipe(east, west)),
-        ('L', Feature::Pipe(north, east)),
-        ('J', Feature::Pipe(north, west)),
-        ('7', Feature::Pipe(south, west)),
-        ('F', Feature::Pipe(south, east)),
-        ('S', Feature::Start),
-        ('.', Feature::Ground),
-    ]);
-
-    let mut start = None;
+pub fn run() -> usize {
     let map = include_str!("day_ten.txt")
         .lines()
-        .enumerate()
-        .map(|(row, line)| {
-            line.chars()
-                .enumerate()
-                .map(|(col, c)| {
-                    let feature = features.get(&c).expect(&format!(
-                        "i was looking for a char and found this '{c}' instead"
-                    ));
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
 
-                    if let Feature::Start = feature {
-                        start = Some([row as isize, col as isize])
-                    }
-
-                    feature
-                })
-                .collect::<Vec<&Feature>>()
-        })
-        .collect::<Vec<Vec<&Feature>>>();
-
-    let start = start.unwrap();
-    let mut layout = vec![vec![0; map.len()]; map.len()];
-    layout[start[0] as usize][start[1] as usize] = 1;
-    let mut stack = vec![
-        (start, [north[0] + start[0], north[1] + start[1]]),
-        (start, [south[0] + start[0], south[1] + start[1]]),
-        (start, [east[0] + start[0], east[1] + start[1]]),
-        (start, [west[0] + start[0], west[1] + start[1]]),
-    ];
-
-    while stack.len() > 0 {
-        let (from, to) = stack.pop().unwrap();
-        if to[0] < 0 || to[1] < 0 {
-            continue;
-        }
-
-        if let Some(row) = map.get(to[0] as usize) {
-            if let Some(Feature::Pipe(first, second)) = row.get(to[1] as usize) {
-                let first = [first[0] + to[0], first[1] + to[1]];
-                let second = [second[0] + to[0], second[1] + to[1]];
-                if first.eq(&from) {
-                    layout[to[0] as usize][to[1] as usize] = 1;
-                    stack.push((to, second));
-                }
-
-                if second.eq(&from) {
-                    layout[to[0] as usize][to[1] as usize] = 1;
-                    stack.push((to, first));
-                }
+    let row_size = map.len();
+    let col_size = map[0].len();
+    let mut enclosed = vec![vec![0; col_size]; row_size];
+    for row in 0..row_size {
+        let mut state = State::Closed;
+        for col in 0..col_size {
+            match map[row][col] {
+                '-' => {}
+                '.' => match state {
+                    State::Open => enclosed[row][col] += 1,
+                    _ => {}
+                },
+                _ => match state {
+                    State::Open => state = State::Closed,
+                    State::Closed => state = State::Open,
+                },
             }
         }
     }
 
-    let mut enclosed = 0;
-    let size = layout.len();
-    for row in 0..size {
-        for col in 0..size {
-            if layout[row][col] == 0 && is_enclosed(row, col, &layout) {
-                enclosed += 1;
+    for col in 0..col_size {
+        let mut state = State::Closed;
+        for row in 0..row_size {
+            match map[row][col] {
+                '|' => {}
+                '.' => match state {
+                    State::Open => enclosed[row][col] += 1,
+                    _ => {}
+                },
+                _ => match state {
+                    State::Open => state = State::Closed,
+                    State::Closed => state = State::Open,
+                },
             }
         }
     }
 
-    println!("{layout:?}");
+    println!("{enclosed:?}");
 
-    *layout.iter().flatten().max().unwrap()
+    enclosed.iter().flatten().filter(|v| 2u32.eq(v)).count()
 }
 
 #[derive(Debug)]
@@ -94,6 +55,7 @@ enum Feature {
     Start,
 }
 
-fn is_enclosed(row: usize, col: usize, layout: &Vec<Vec<u32>>) -> bool {
-    false
+enum State {
+    Open,
+    Closed,
 }
